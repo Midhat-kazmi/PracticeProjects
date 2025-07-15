80f51b4e8f7bdbe3ef539ebb64ca70194c45b613
Midhat-dot <midhatcodes@gmail.com>
2025-07-14 21:44:51 -0700

    update in counter

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
  }
   return (
     <>
-     <h1>Chai aur react</h1>
+     <h1>Midhats counter</h1>
      <h2>Counter value : {counter}</h2>
      
      <button onClick={addValue}>Add Value</button>
